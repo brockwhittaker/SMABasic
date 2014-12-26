e9a69ee4c03adeb20d2b02fbd8a077e1764df5c1
Brock Whittaker <brockwhittaker@Brocks-MacBook-Pro.local>
2014-12-26 14:14:48 -0700

    Initial Commit

---
+++ SMABasic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SMABasic
+//
+//  Created by Brock Whittaker on 12/26/14.
+//  Copyright (c) 2014 Brock Whittaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
